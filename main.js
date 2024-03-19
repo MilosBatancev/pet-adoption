ce15ec0c94627993689f9c5755ce4c70c87cd980
Milos <milosbatancev870@gmail.com>
2024-03-19 20:47:02 +0100

    Finished pet filtering

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ async function petsArea() {
   const petsData = await petsPromise.json();
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true);
+    clone.querySelector('.pet-card').dataset.species = pet.species;
     clone.querySelector('h3').textContent = pet.name;
     clone.querySelector('.pet-description').textContent = pet.description;
     clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear);
@@ -47,4 +48,12 @@ allButtons.forEach(el => {
 function handleButtonClick(e) {
   allButtons.forEach(el => el.classList.remove("active"));
   e.target.classList.add('active');
+  const currentFilter = e.target.dataset.filter;
+  document.querySelectorAll('.pet-card').forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == 'all') {
+      el.style.display = 'grid';
+    } else {
+      el.style.display = 'none';
+    }
+  })
 }
