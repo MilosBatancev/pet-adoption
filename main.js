bf65130ceab1097badb3c89670f5b910379106ad
Milos <milosbatancev870@gmail.com>
2024-03-24 20:51:06 +0100

    Pull in a real dynamic data.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function start() {
 
 start();
 async function petsArea() {
-  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+  const petsPromise = await fetch("https://luminous-travesseiro-3722d1.netlify.app/.netlify/functions/pets");
   const petsData = await petsPromise.json();
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true);
